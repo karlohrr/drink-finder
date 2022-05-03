147855b51aadfb8469d5175a18db2e23be18d065
Christopher Patlingrao <cpatlingrao@gmail.com>
2022-05-02 19:47:59 -0800

    Drink get call working

--- src/index.js
+++ src/index.js
@@ -3,29 +3,58 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import IngredientList from "./components/IngredientList";
 import GetDrinksButton from "./components/GetDrinksButton";
+import { config } from "./config.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedIngredient: null,
+      drinks: null,
     };
   }
 
   ingredientSelect = (selectedIngredient) => {
     this.setState({ selectedIngredient });
-    console.log(selectedIngredient);
   };
 
   getDrinks = () => {
-    console.log("getting drinks");
+    const ingredient = this.state.selectedIngredient?.value ?? null;
+    let message;
+    if (!ingredient) {
+      message = "no ingredient selected.";
+
+      this.setState({
+        drinks: message,
+      });
+    } else {
+      message = `getting drinks for ingredient: ${ingredient}`;
+      let fullURL = `${config.baseURL}filter.php?i=${ingredient}`;
+      fetch(fullURL)
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then(
+          (result) => {
+            this.setState({
+              drinks: result.drinks[0].strDrink,
+            });
+            console.log(result.drinks[0]);
+          },
+          (error) => {
+            console.log("shit broke");
+          }
+        );
+    }
   };
 
   render() {
     return (
       <div>
         <IngredientList onChange={this.ingredientSelect}></IngredientList>
-        <GetDrinksButton></GetDrinksButton>
+        <GetDrinksButton onClick={this.getDrinks}></GetDrinksButton>
+        <div>{this.state.drinks}</div>
       </div>
     );
   }
